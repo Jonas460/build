92e3de008174faa158a480f9f3284d17cde53968
Jonas460 <70544178+Jonas460@users.noreply.github.com>
2021-11-03 00:57:41 -0300

    Add files via upload

---
+++ precache-manifest.45389c499566240c703f8ea9792626b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1207750b1a538ea2570b04e04cfaf5e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b21b5fabd664b3cfd2a",
+    "url": "./static/css/main.8378cb35.chunk.css"
+  },
+  {
+    "revision": "71fa3ae9a6178ce9dec7",
+    "url": "./static/js/2.593c7741.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/2.593c7741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e823061ec7b20a577c36",
+    "url": "./static/js/3.c99467bb.chunk.js"
+  },
+  {
+    "revision": "54ece9a7734413b9e0ccfc843a5c465b",
+    "url": "./static/js/3.c99467bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3aedab8ff57370d960",
+    "url": "./static/js/4.c09e87fd.chunk.js"
+  },
+  {
+    "revision": "aa0d01a96b90795adfcac80081b04497",
+    "url": "./static/js/4.c09e87fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3492bdaaf918642890e",
+    "url": "./static/js/5.e081f0d6.chunk.js"
+  },
+  {
+    "revision": "54a13f759b1799645c0f4c440498a75c",
+    "url": "./static/js/5.e081f0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b21b5fabd664b3cfd2a",
+    "url": "./static/js/main.ec7f63c0.chunk.js"
+  },
+  {
+    "revision": "a2320fc3bf095e20780d",
+    "url": "./static/js/runtime-main.56cb7f93.js"
+  },
+  {
+    "revision": "e9e061637a6e8f37a242170ebd00945e",
+    "url": "./static/media/LASMCE.e9e06163.png"
+  }
+]);
